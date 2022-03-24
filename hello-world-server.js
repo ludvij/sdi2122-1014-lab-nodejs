289b33d915837c5bc6bf7595dddf5e80726de191
ludvij <vijande48@gmail.com>
2022-03-24 01:34:20 +0100

    SDI-2122-1014-7.2-Express, depuración y variables de entorno.

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,4 +1,6 @@
-let http = require('http')
+
+const http = require('http')
+
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
     res.end('hello world!\n')
