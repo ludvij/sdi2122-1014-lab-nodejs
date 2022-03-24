e6f99e28a9dba93c50742c0789dd2452770f5ebe
ludvij <vijande48@gmail.com>
2022-03-24 02:32:19 +0100

    SDI-2122-1014-7.3-División de las rutas en módulos, peticiones GET y parametros

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,5 +1,5 @@
 
-module.exports = function (app) {
+module.exports = (app) => {
     app.get("/songs", (req, res) => {
         let response = ""
         if (req.query.title != null && typeof (req.query.title) != "undefined")
@@ -9,4 +9,19 @@ module.exports = function (app) {
 
         res.send(response)
     })
+    app.get('/add', (req, res) => {
+        let response = parseInt(req.query.num1) + parseInt(req.query.num2)
+
+        res.send(response)
+    })
+    app.get('/songs/:id', (req, res) => {
+        let response = 'id: ' + req.params.id
+        res.send(response)
+    })
+    app.get('/songs/:kind/:id', (req, res) => {
+        let response = 'id: ' + req.params.id + '<br>'
+            + 'Tipo de música: ' + req.params.kind
+        res.send(response)
+    })
 }
+
