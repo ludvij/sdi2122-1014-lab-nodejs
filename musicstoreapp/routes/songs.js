e28c33670c2721b713f4870103a3cb3f2e7960ae
ludvij <vijande48@gmail.com>
2022-03-26 03:39:23 +0100

    SDI-2122-1014-7.5-Vistas y motores de plantillas.

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,18 +1,29 @@
 
 module.exports = (app) => {
     app.get("/songs", (req, res) => {
-        let response = ""
-        if (req.query.title != null && typeof (req.query.title) != "undefined")
-            response = 'Titulo: '+ req.query.title + '<br>'
-        if (req.query.author != null && typeof (req.query.author) != "undefined")
-            response += 'Author: ' + req.query.author
-
-        res.send(response)
+        let songs = [{
+            "title": "Blank space",
+            "price": "1.2"
+        },{
+            "title": "See you again",
+            "price": "1.3"
+        }, {
+            "title": "Uptown Funk",
+            "price": "1.1"
+        }]
+        let response = {
+            seller: "Tienda de canciones",
+            songs: songs
+        }
+        res.render("shop.twig", response)
     })
     app.get('/add', (req, res) => {
         let response = parseInt(req.query.num1) + parseInt(req.query.num2)
 
         res.send(response)
+    })
+    app.get('/songs/add', (req, res) => {
+        res.render("add.twig");
     })
     app.get('/songs/:id', (req, res) => {
         let response = 'id: ' + req.params.id
