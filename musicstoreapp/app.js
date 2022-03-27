903f5aca9b603e62f5eb916c5c936e40b53f5c48
ludvij <vijande48@gmail.com>
2022-03-27 21:44:36 +0200

    SDI-2122-1014-7.101-Ejercicio complementario 1

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 require("./routes/songs.js")(app)
+require("./routes/authors.js")(app)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ musicstoreapp/routes/authors.js
@@ -0,0 +1,45 @@
+module.exports = (app) => {
+    app.get('/authors/add', (req, res) => {
+        res.render("authors/add.twig");
+    })
+    app.post('/authors/add', (req, res) => {
+        let response = ''
+        if (req.body.name != null && typeof (req.body.name) != 'undefined')
+            response += 'Autor agregado: ' + req.body.name + '<br>'
+        else
+            response += 'Nombre no enviado en la petición'
+        if (req.body.group != null && typeof (req.body.group) != 'undefined')
+            response += 'Grupo: ' + req.body.group + '<br>'
+        else
+            response += 'Grupo no enviado en la petición'
+        if (req.body.kind != null && typeof (req.body.kind) != 'undefined')
+            response += 'Rol: ' + req.body.kind
+        else
+            response += 'Rol no enviado en la petición'
+
+        res.send(response)
+    })
+    app.get('/authors', (req, res) => {
+        let authors = [{
+            "name": "test name",
+            "group": "test group",
+            'kind': 'singer'
+        },{
+            "name": "test name",
+            "group": "test group",
+            'kind': 'singer'
+        }, {
+            "name": "test name",
+            "group": "test group",
+            'kind': 'singer'
+        }]
+        let response = {
+            seller: "Autores",
+            authors: authors
+        }
+        res.render("authors/authors.twig", response)
+    })
+    app.get('/authors/*', (req, res) => {
+        res.redirect('/authors')
+    })
+}
