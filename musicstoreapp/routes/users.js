01be52ac94eb88ac66d9861e99ce901afc330b8a
Ludvij <Luisvijande@gmail.com>
2022-04-04 12:21:46 +0200

    SDI-2122-1014-9.3-Mensajes y alertas.

--- musicstoreapp/routes/users.js
+++ musicstoreapp/routes/users.js
@@ -12,28 +12,33 @@ module.exports = function (app, usersRepository) {
 		req.session.user = null;
 		res.send('El usuario se ha desconectado correctamente');
 	})
-	app.post('/users/login', (req, res) => {
-		let securePassword = app.get('crypto').createHmac('sha256', app.get('clave'))
-			.update(req.body.password).digest('hex')
+	app.post('/users/login', function (req, res) {
+		let securePassword = app.get("crypto").createHmac('sha256', app.get('clave'))
+			.update(req.body.password).digest('hex');
 		let filter = {
-
 			email: req.body.email,
 			password: securePassword
 		}
-		let options = {}
-		usersRepository.findUser(filter, options)
-			.then(user => {
-				if (user == null) {
-					req.session.user = null
-					res.send('Usuario no encontrado')
-				} else {
-					req.session.user = user.email
-					res.redirect('/publications')
-				}
-			}).catch(error => {
-				req.session.user = null
-				res.send('Se ha producido un error al buscar el usuario: ' + error)
-			})
+		let options = {};
+		usersRepository.findUser(filter, options).then(user => {
+			if (user == null) {
+				req.session.user = null;
+				//res.send("Usuario no identificado");
+				res.redirect("/users/login" +
+					"?message=Email o password incorrecto"+
+					"&messageType=alert-danger ");
+			} else {
+				req.session.user = user.email;
+				//res.send("Usuario identificado correctamente: " + user.email
+				res.redirect("/publications");
+			}
+		}).catch(error => {
+			req.session.user = null;
+			//res.send("Se ha producido un error al buscar el usuario: " + error)
+			res.redirect("/users/login" +
+				"?message=Se ha producido un error al buscar el usuario"+
+				"&messageType=alert-danger ");
+		})
 	})
 	app.post('/users/signup', function (req, res) {
 		let securePassword = app.get('crypto').createHmac('sha256', app.get('clave'))
@@ -43,9 +48,9 @@ module.exports = function (app, usersRepository) {
 			password: securePassword
 		}
 		usersRepository.insertUser(user).then(userId => {
-			res.redirect('/users/login')
+			res.redirect('/users/login?message=Nuevo usuario registrado.&messageType=alert-info')
 		}).catch(error => {
-			res.send('Error al insertar el usuario');
+			res.redirect('/users/signup?message=Se ha producido un error al registrar el usuario.&messageType=alert-danger')
 		});
 	});
 }
